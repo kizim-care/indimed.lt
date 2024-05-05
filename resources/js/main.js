c863d1a74c16eaf4f75b2e379216c9d00e3177fe
Sergej <23sergej@gmail.com>
2024-05-05 21:10:03 +0300

    WIP

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,17 +2,62 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('mainForm', (data) => ({
         fields: {},
         conditionalFields: {},
+        hasErrors: false,
+        errorMessages: [],
         post() {
-            console.log('post', this.fields);
+            const emptyValidationFields = ['patientAge', 'description', 'canArrive', 'name', 'email', 'phone']
+
+            for(let i = 0; i < emptyValidationFields.length; i++) {
+                if (this.fields[emptyValidationFields[i]].value === '') {
+                    this.fields[emptyValidationFields[i]].error = true
+
+                    this.hasErrors = true
+
+                    this.errorMessages.push(this.fields[emptyValidationFields[i]].errorMessage)
+                }
+            }
+
+            if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(this.fields.email.value)) {
+                this.fields.email.error = true
+
+                this.hasErrors = true
+
+                if (!this.errorMessages.includes(this.fields.email.errorMessage)) {
+                    this.errorMessages.push(this.fields.email.errorMessage)
+                }
+            }
+
+            if (!/^(\+3706|86|003706|03706|3706|6|06)\d{7}$/.test(this.fields.phone.value)) {
+                this.fields.phone.error = true
+
+                this.hasErrors = true
+
+                if (!this.errorMessages.includes(this.fields.phone.errorMessage)) {
+                    this.errorMessages.push(this.fields.phone.errorMessage)
+                }
+            }
+
+            if (this.hasErrors) {
+                return
+            }
+
+            console.log('Submit form');
+        },
+        revalidateField(name) {
+            this.fields[name].error = false
+
+            this.hasErrors = false
+
+            this.errorMessages = []
         },
         show(name) {
             if (!this.conditionalFields.hasOwnProperty(name)) {
-                return false;
+                return false
             }
 
             for(let i = 0; i < this.conditionalFields[name].length; i++) {
-                if (this.fields[this.conditionalFields[name][i].field.data.name] !== this.conditionalFields[name][i].value) {
-                    return false;
+                if (this.fields[this.conditionalFields[name][i].field.data.name].value !== this.conditionalFields[name][i].value) {
+                    return false
                 }
             }
 
@@ -20,13 +65,16 @@ document.addEventListener('alpine:init', () => {
         },
         init() {
             data.forEach((field) => {
-                this.fields[field.data.name] = field.data.value ?? '';
+                this.fields[field.data.name] = {
+                    value: field.data.value ?? '',
+                    error: false,
+                    errorMessage: field.data.errorMessage,
+                }
 
                 if (field.data.hasOwnProperty('conditions')) {
-                    this.conditionalFields[field.data.name] = field.data.conditions;
+                    this.conditionalFields[field.data.name] = field.data.conditions
                 }
-            });
-            console.log('init', this.conditionalFields);
+            })
         }
-    }));
+    }))
 })
--- src/_data/_formFields.js
+++ src/_data/_formFields.js
@@ -28,6 +28,7 @@ const patientAge = {
                 text: _const.patientOver60,
             }
         ],
+        errorMessage: 'Prašome pasirinkti paciento amžiaus grupę',
         icon: '<svg class="h-5 w-5" stroke-width="2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" data-slot="icon"><path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"></path></svg>',
     },
 }
@@ -38,6 +39,7 @@ const description = {
         name: 'description',
         label: 'Trumpas Jūsų atvejo aprašymas',
         placeholder: 'Trumpai aprašykite savo atvejį',
+        errorMessage: 'Prašome aprašyti savo atvejį',
     },
 }
 
@@ -61,6 +63,7 @@ const canArrive = {
                 text: _const.canArriveUnknown,
             },
         ],
+        errorMessage: 'Prašome pasirinkti ar galite atvykti pas specialistą',
         icon: '<svg class="h-5 w-5" stroke-width="2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" data-slot="icon"><path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"></path></svg>',
     },
 }
@@ -72,6 +75,7 @@ const name = {
         type: 'text',
         label: 'Jūsų vardas',
         placeholder: 'Įveskite savo vardą',
+        errorMessage: 'Prašome įvesti savo vardą',
         icon: '<svg class="h-5 w-5" stroke-width="2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" data-slot="icon"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"></path></svg>',
     },
 }
@@ -83,6 +87,7 @@ const email = {
         type: 'email',
         label: 'Jūsų el. pašto adresas',
         placeholder: 'Įveskite savo el. pašto adresą',
+        errorMessage: 'Prašome įvesti savo el. pašto adresą',
         icon: '<svg class="h-5 w-5" stroke-width="2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" data-slot="icon"><path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"></path></svg>',
     },
 }
@@ -94,6 +99,7 @@ const phone = {
         type: 'tel',
         label: 'Jūsų telefono numeris',
         placeholder: 'Įveskite savo telefono numerį',
+        errorMessage: 'Prašome įvesti savo telefono numerį',
         icon: '<svg class="h-5 w-5" stroke-width="2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" data-slot="icon"><path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"></path></svg>',
     },
 }

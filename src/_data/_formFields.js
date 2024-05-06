4e63dcc05f3569c9c7206108798d92b8761a054e
Sergej O <31093331+23sergej@users.noreply.github.com>
2024-05-06 18:59:32 +0300

    Add city to the form (#4)

--- src/_data/_formFields.js
+++ src/_data/_formFields.js
@@ -1,11 +1,64 @@
 const _const = require('./_const');
 
-const patientAge = {
-    type: "select",
+const city = {
+    type: 'select',
     data: {
-        name: "patientAge",
-        label: "Paciento amžiaus grupė",
-        placeholder: "Pasirinkite amžiaus grupę",
+        name: 'city',
+        label: 'Miestas',
+        placeholder: 'Pasirinkite miestą',
+        options: [
+            {
+                value: _const.cityVilnius,
+                text: _const.cityVilnius,
+            },
+            {
+                value: _const.cityKaunas,
+                text: _const.cityKaunas,
+            },
+            {
+                value: _const.cityKlaipeda,
+                text: _const.cityKlaipeda,
+            },
+            {
+                value: _const.citySiauliai,
+                text: _const.citySiauliai,
+            },
+            {
+                value: _const.cityPanevezys,
+                text: _const.cityPanevezys,
+            },
+            {
+                value: _const.cityAlytus,
+                text: _const.cityAlytus,
+            },
+            {
+                value: _const.cityMarijampole,
+                text: _const.cityMarijampole,
+            },
+            {
+                value: _const.cityUtena,
+                text: _const.cityUtena,
+            },
+            {
+                value: _const.cityTaurage,
+                text: _const.cityTaurage,
+            },
+            {
+                value: _const.cityTelsiai,
+                text: _const.cityTelsiai,
+            },
+        ],
+        errorMessage: 'Prašome pasirinkti miestą',
+        icon: '<svg class="h-5 w-5" stroke-width="2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" /></svg>',
+    }
+}
+
+const patientAge = {
+    type: 'select',
+    data: {
+        name: 'patientAge',
+        label: 'Paciento amžiaus grupė',
+        placeholder: 'Pasirinkite amžiaus grupę',
         options: [
             {
                 value: _const.patientUnder1,
@@ -107,6 +160,7 @@ const phone = {
 module.exports = {
     patientAge,
     description,
+    city,
     canArrive,
     name,
     email,

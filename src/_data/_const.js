9bf18c38ce630010c5403f09cd8fc4b0a155a1b4
Jegor <je.menkov@gmail.com>
2024-05-14 09:38:33 +0300

    Text changes

--- src/_data/_const.js
+++ src/_data/_const.js
@@ -1,13 +1,13 @@
 module.exports = {
     patientUnder1: 'Kūdikis - iki 1 metų',
-    patientUnder7: 'Vaikas - iki 7 metų',
-    patientUnder18: 'Paauglys - iki 18 metų',
-    patientUnder60: 'Suaugęs - iki 60 metų',
+    patientUnder7: 'Vaikas - nuo 1 iki 7 metų',
+    patientUnder18: 'Paauglys - nuo 7 iki 18 metų',
+    patientUnder60: 'Suaugęs - nuo 18 metų iki 60 metų',
     patientOver60: 'Senjoras - 60+ metų',
 
-    canArriveYes: 'Taip > Galiu atvykti nurodytu adresu',
-    canArriveNo: 'Ne > Reikia specialisto į namus',
-    canArriveUnknown: 'Nežinau > Noriu pasitarti su specialistu',
+    canArriveYes: 'Taip - Galiu atvykti nurodytu adresu',
+    canArriveNo: 'Ne - Noriu iškviesti specialistą į namus',
+    canArriveUnknown: 'Kita - Noriu pasitarti su specialistu',
 
     cityVilnius: 'Vilnius',
     cityKaunas: 'Kaunas',
